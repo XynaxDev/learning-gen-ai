9ed2040f29eb2cceee1686c3320817fa0a772706
Akash Kumar <akashkumar.cs27@gmail.com>
2026-01-06 02:30:54 +0530

    ADD: textLoader file and supporting txt file

---
+++ IndexesRAG/DocumentLoaders/textLoader.py
@@ -0,0 +1,27 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda
+
+model = ChatOllama(model="llama3.2:3b", temperature=0.6)
+
+prompt = PromptTemplate(
+    template="Summarise the given text with their key concepts: {text}",
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+# loading the data
+loader = TextLoader("./IndexesRAG/DocumentLoaders/nolan.txt", encoding='utf-8')
+docs = loader.load()
+# fetching the content
+loader2 = RunnableLambda(lambda x: x.load(), lambda y: y[0].page_content)
+docs_content = docs[0].page_content
+
+chain = prompt | model | parser
+
+summary = chain.invoke({'text': docs_content})
+
+print(summary)
